2ca133e1ff3c035732c7660ddfe8afd45a491cbf
kukold <vlados.18@bk.ru>
2023-11-11 23:49:03 +0700

    +postitem

---
+++ src/components/PostItem.vue
@@ -0,0 +1,22 @@
+<template>
+ <div class="post">
+    <div><strong>Hазвание:</strong> {{ post.title }}</div>
+    <div><strong>Oписание:</strong> {{ post.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostItem",
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,13 +1,16 @@
 <template>
- <div class="post" v-for="post in posts">
-    <div><strong>Hазвание:</strong> {{ post.title }}</div>
-    <div><strong>Oписание:</strong> {{ post.body }}</div>
+  <div>
+    <h3>Список пользователей</h3>
+    <post-item v-for="post in posts" :post="post"/>
   </div>
 </template>
 
 <script>
+import PostItem from "@/components/PostItem.vue";
+
 export default {
   name: "PostList",
+  components: {PostItem},
   props: {
     posts:{
       type: Array,
